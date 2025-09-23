ce69f500324a71729baa2e1bf640218bdb8e49c2
Robel <robel.getaneh27@gmail.com>
2025-09-23 15:01:41 +0300

    admin url added

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: process.env.Client_Url || "*",
+  origin: process.env.Client_Url || process.env.Admin_Url,
   credentials: true,
 }));
 
